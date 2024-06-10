0127eed4dc78a7b8e68cc3a8eaf6b717b6738232
Levi Starrett <levi@roxsoftware.com>
2024-06-10 12:10:02 -0400

    Board representation working for all test cases

--- lichess_bot/gen/gen_tests.py
+++ lichess_bot/gen/gen_tests.py
@@ -1,12 +1,16 @@
 import chess
 import chess.pgn
+import sys
+
+parent_package_name = 'lichess_bot::components::Engine::test'
+package_name = 'do_move_test_cases'
 
 # parse all the games
 test_games = []
-with open('/Users/levi/Downloads/lichess_leviathan747_2024-06-10.pgn') as pgn:
+with open(sys.argv[1]) as pgn:
     game = chess.pgn.read_game(pgn)
     while game is not None:
-        test_game = {'moves': [], 'fens': []}
+        test_game = {'moves': [], 'fens': [], 'initial_fen': game.headers.get('FEN', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')}
         board = game.board()
         for move in game.mainline_moves():
             board.push(move)
@@ -16,7 +20,15 @@ with open('/Users/levi/Downloads/lichess_leviathan747_2024-06-10.pgn') as pgn:
             test_games.append(test_game)
         game = chess.pgn.read_game(pgn)
 
-# output test operations
+# output file header
+print('// BP 7.1.6 content: Package syschar: 3 persistence-version: 7.1.6')
+print()
+print(f'within {parent_package_name} is')
+print()
+print(f'\tpackage {package_name} is')
+print()
+
+# output test functions
 for test_num, test_game in enumerate(test_games):
     print('\t\t@dialect("oal");')
     print(f'\t\tfunction test{test_num + 1}() return boolean is')
@@ -24,12 +36,12 @@ for test_num, test_game in enumerate(test_games):
     print(f'\t\t\tLOG::LogInfo(message: "test{test_num + 1}: start");')
     print('\t\t\tt0 = TIM::current_clock();')
     print()
-    print('\t\t\tinitial_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";')
+    print(f'\t\t\tinitial_fen = "{test_game["initial_fen"]}";')
     print(f'\t\t\tmove_text = "{" ".join(test_game["moves"])}";')
     for i in reversed(range(len(test_game['fens']))):
         print(f'\t\t\tfens[{i}] = "{test_game["fens"][i]}";')
     print()
-    print(f'\t\t\tresult = ::test_do_move(name: "test{test_num}", initial_fen: initial_fen, move_text: move_text, fens: fens);')
+    print(f'\t\t\tresult = ::test_do_move(name: "test{test_num + 1}", initial_fen: initial_fen, move_text: move_text, fens: fens);')
     print()
     print('\t\t\tt1 = TIM::current_clock();')
     print(f'\t\t\tLOG::LogInfo(message: "test{test_num + 1}: done " + STRING::itoa(i: (t1 - t0) / 1000) + "ms");')
@@ -37,3 +49,27 @@ for test_num, test_game in enumerate(test_games):
     print('\t\t\treturn result;')
     print('\t\t\t@endnoparse')
     print('\t\tend function;')
+    print()
+
+# output test_all function
+print('\t\t@dialect("oal");')
+print('\t\tfunction test_all() is')
+print('\t\t\t@noparse')
+print('\t\t\tfailures = 0;')
+print('\t\t\tt0 = TIM::current_clock();')
+print()
+for test_num, test_game in enumerate(test_games):
+    print(f'\t\t\tif not ::test{test_num + 1}() then failures = failures + 1; end if;')
+print()
+print('\t\t\tt1 = TIM::current_clock();')
+print(f'\t\t\tLOG::LogInfo(message: "{len(test_games)} tests run in " + STRING::itoa(i: (t1 - t0) / 1000) + "ms");')
+print('\t\t\tLOG::LogInfo(message: "Tests ran with " + STRING::itoa(i: failures) + " failures");')
+print('\t\t\tARCH::shutdown();')
+print('\t\t\t@endnoparse')
+print('\t\tend function;')
+print()
+
+# ouptut file footer
+print('\tend package;')
+print()
+print('end;')
